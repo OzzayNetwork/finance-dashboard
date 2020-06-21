5709ad66f281b42b3f1286e6c5ee8dfb6e7e79fe
ozzaynetworks <ozzaynetworks@gmail.com>
2020-06-21 18:38:52 +0300

    graphs

---
+++ js/chart-data/revenue-streams.js
@@ -0,0 +1,153 @@
+Highcharts.chart('RevenueStreams', {
+    chart: {
+        type: 'column',
+		backgroundColor: 'transparent'
+				
+    },
+    title: {
+        text: 'Collection by Revenue stream',
+		style:{
+			color: '#a5a8ad'
+		}
+    },
+    xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    yAxis: {
+        gridLineColor: '#197F07',
+        gridLineWidth: 0.3,
+        min: 0,
+        title: {
+            text: 'Total revenue collected'
+        },
+        stackLabels: {
+            enabled: false,
+            style: {
+                fontWeight: 'bold',
+               color: '#a5a8ad'
+            }
+        }
+    },
+    legend: {
+        align: 'right',
+        x: -30,
+        verticalAlign: 'top',
+        y: 25,
+        floating: true,
+        backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || 'white',
+        borderColor: '#CCC',
+        borderWidth: 1,
+        shadow: false
+    },
+	legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'bottom',
+            borderWidth: 0,
+			backgroundColor:'transparent',
+			borderColor: '#CCC',
+			borderWidth: 1,
+			itemStyle: {
+            color: 'white',
+			font: '600 10px "Muli", sans-serif'
+        },
+        itemHoverStyle: {
+            color: 'white',
+			font: '600 10px "Muli", sans-serif'
+        },
+        },
+    tooltip: {
+        headerFormat: '<h3><strong class="mb-3" style="font-size: 20px !important;">{point.x}</strong></h3><br/><br/><br/>',
+        /*  pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}', */
+        pointFormat: '<strong style="color:{series.color}; font-weight:bold;">{series.name}</strong>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/><br/>',
+        shared: true,
+
+
+    },
+
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            grouping: false,
+            dataLabels: {
+                enabled: false
+            },
+            states: {
+                hover: {
+                    enabled: false
+                }
+            },
+            point: {
+                events: {
+                    mouseOver: updateStackColor(0.2),
+                    mouseOut: updateStackColor(0)
+                }
+            }
+
+        },
+        series: {
+            //connectNulls: true
+
+            pointWidth: 10,
+            borderWidth: 0,
+            borderColor: 'black',
+
+
+        },
+    },
+    series: [{
+        name: 'Parking Fees',
+        data: [5, 3, 4, 7, 2, 5, 3, 4, 7, 2, 5, 3]
+    }, {
+        name: 'Billboards and Advertisements',
+        data: [2, 2, 3, 2, 1, 2, 2, 3, 2, 1, 2, 2]
+    }, {
+        name: 'Other incomes',
+        data: [3, 4, 4, 2, 5, 3, 4, 4, 2, 5, 3, 4]
+    }, {
+        name: 'Market Fees',
+        data: [3, 4, 4, 2, 5, 3, 4, 4, 2, 5, 3, 4]
+    }, {
+        name: 'Single Business Permits',
+        data: [3, 4, 4, 2, 5, 3, 4, 4, 2, 5, 3, 4]
+    }, {
+        name: 'Land Rates',
+        data: [3, 4, 4, 2, 5, 3, 4, 4, 2, 5, 3, 4]
+    }, {
+        name: 'Plans and Inspections',
+        data: [3, 4, 4, 2, 5, 3, 4, 4, 2, 5, 3, 4]
+    }, {
+        name: 'Fire Inspection Certificate',
+        data: [2, 5, 3, 4, 3, 4, 4, 4, 2, 5, 3, 4]
+    }, {
+        name: 'House and stall rents',
+        data: [5, 3, 4, 3, 4, 4, 4, 2, 2, 5, 3, 4]
+    }]
+});
+//		hover effect
+function updateStackColor(alpha) {
+    return function () {
+        const x = this.x
+        const color = Highcharts.color
+        const colors = Highcharts.getOptions().colors
+
+        this.series.chart.series.forEach((series, i) => {
+            series.data.forEach(point => {
+                const basePointColor = color(colors[i])
+
+                point.update({
+                    color: alpha === 0
+                        ? basePointColor.get() // set original color
+                        : point.x === x
+                        ? basePointColor.brighten(-alpha).get() // brighten original color
+                        : basePointColor.brighten(alpha).get() // dim orignal color
+                }, false)
+            })
+        })
+
+        this.series.chart.redraw(false)
+    }
+}
+
+
+//		hover effect
